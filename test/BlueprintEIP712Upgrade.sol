90667547e013455994689e075490c8b809b7b826
spidemen <xingyuan15@gmail.com>
2025-01-15 10:12:49 +0800

    add eip712 upgrade test

---
+++ test/BlueprintEIP712Upgrade.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {BlueprintEIP712Upgrade} from "../src/BlueprintEIP712Upgrade.sol";
+import {stdError} from "forge-std/StdError.sol";
+import {BlueprintV1} from "../src/BlueprintV1.sol";
+import {BlueprintUpgradeTest} from "../src/BlueprintUpgradeTest.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {BlueprintV2} from "../src/BlueprintV2.sol";
+import {BlueprintV3} from "../src/BlueprintV3.sol";
+
+contract EIP712Test is Test {
+    BlueprintV1 public proxy;
+    BlueprintV3 public proxyV3;
+    bytes32 public projectId;
+
+    function setUp() public {
+        BlueprintV1 blueprint = new BlueprintV1();
+
+        // Create a proxy pointing to the implementation
+        ERC1967Proxy e1967 = new ERC1967Proxy(address(blueprint), abi.encodeWithSignature("initialize()"));
+
+        // Interact with the proxy as if it were the implementation
+        proxy = BlueprintV1(address(e1967));
+
+        BlueprintV2 blueprintV2 = new BlueprintV2();
+        proxy.upgradeToAndCall(address(blueprintV2), abi.encodeWithSignature("initialize()"));
+        string memory ver = proxy.VERSION();
+        assertEq(ver, "2.0.0");
+
+        BlueprintV3 blueprintV3 = new BlueprintV3();
+        proxy.upgradeToAndCall(address(blueprintV3), abi.encodeWithSignature("initialize()"));
+        ver = proxy.VERSION();
+        assertEq(ver, "3.0.0");
+
+        // Interact with the proxy as if it were the implementation
+        proxyV3 = BlueprintV3(address(proxy));
+
+        projectId = bytes32(0x2723a34e38d0f0aa09ce626f00aa23c0464b52c75516cf3203cc4c9afeaf2980);
+    }
+
+    function test_getRequestProposalDigest() public {
+        string memory base64RecParam = "data:image/png;base64,sdfasdfsdf";
+        string memory serverURL = "https://example.com";
+
+        // Generate the hash of the request proposal from old eip712
+        bytes32 digest1 = proxyV3.getRequestProposalDigest(projectId, base64RecParam, serverURL);
+
+        BlueprintEIP712Upgrade blueprintEIP712Upgrade = new BlueprintEIP712Upgrade();
+        proxy.upgradeToAndCall(address(blueprintEIP712Upgrade), abi.encodeWithSignature("initialize()"));
+        string memory ver = proxy.VERSION();
+        assertEq(ver, "3.0.0");
+        proxyV3 = BlueprintV3(address(proxy));
+        // Generate the hash of the request proposal from new eip712
+        bytes32 digest2 = proxyV3.getRequestProposalDigest(projectId, base64RecParam, serverURL);
+        assertEq(digest1, digest2);
+    }
+}
