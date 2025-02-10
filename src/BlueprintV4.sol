13bb95839f85c5129d4145ff36484844bab869d7
spidemen <xingyuan15@gmail.com>
2025-02-10 16:38:15 +0800

    add blueprintv4

--- src/BlueprintV4.sol
+++ src/BlueprintV4.sol
@@ -7,15 +7,16 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/cryptography/EIP712Upgradeable.sol";
 import "./Blueprint.sol";
 
-contract BlueprintV3 is Initializable, UUPSUpgradeable, OwnableUpgradeable, Blueprint {
+contract BlueprintV4 is Initializable, UUPSUpgradeable, OwnableUpgradeable, Blueprint {
     string public constant SIGNING_DOMAIN = "app.crestal.network";
+    address public constant NFT_Contract_Address = address(0x7D8be0Dd8915E3511fFDDABDD631812be824f578);
 
-    function initialize() public reinitializer(3) {
+    function initialize() public reinitializer(4) {
         __Ownable_init(msg.sender);
         VERSION = "4.0.0";
         __EIP712_init(SIGNING_DOMAIN, VERSION);
         __UUPSUpgradeable_init();
-        NFTContractAddress = msg.sender;
+        NFTContractAddress = NFT_Contract_Address;
     }
 
     // The _authorizeUpgrade function is required by the UUPSUpgradeable contract
