41f57d8439a81115108fbf065a367a757cc808aa
spidemen <xingyuan15@gmail.com>
2025-03-07 10:33:03 +0800

    add check on transfer toAddress

--- src/Payment.sol
+++ src/Payment.sol
@@ -25,6 +25,7 @@ contract Payment {
     function payWithERC20(address erc20TokenAddress, uint256 amount, address fromAddress, address toAddress) public {
         // check from and to address
         require(fromAddress != toAddress, "Cannot transfer to self address");
+        require(toAddress != address(0), "Invalid to address");
         require(amount > 0, "Amount must be greater than 0");
         IERC20 token = IERC20(erc20TokenAddress);
         token.safeTransferFrom(fromAddress, toAddress, amount);
