cfb1e6a302005666b3de440168f1e3284915dca5
spidemen <xingyuan15@gmail.com>
2025-05-26 15:00:58 +0800

    add updateHash into update flow

--- src/BlueprintCore.sol
+++ src/BlueprintCore.sol
@@ -145,7 +145,11 @@ contract BlueprintCore is EIP712, Payment {
     );
 
     event UpdateDeploymentConfig(
-        bytes32 indexed projectID, bytes32 indexed requestID, address workerAddress, string base64Config
+        bytes32 indexed projectID,
+        bytes32 indexed requestID,
+        address workerAddress,
+        bytes32 updateHash,
+        string base64Config
     );
 
     event CreateAgent(
@@ -666,8 +670,11 @@ contract BlueprintCore is EIP712, Payment {
             }
         }
 
+        bytes32 updateHash =
+            keccak256(abi.encodePacked(block.timestamp, userAddress, requestID, updatedBase64Config, block.chainid));
+
         emit UpdateDeploymentConfig(
-            projectId, requestID, requestDeploymentStatus[requestID].deployWorkerAddr, updatedBase64Config
+            projectId, requestID, requestDeploymentStatus[requestID].deployWorkerAddr, updateHash, updatedBase64Config
         );
     }
 
@@ -775,10 +782,6 @@ contract BlueprintCore is EIP712, Payment {
 
     function getEIP712ContractAddress() public view returns (address) {
         return getAddress();
-    }
-
-    function isWhitelistUser(address userAddress) public view returns (bool) {
-        return whitelistUsers[userAddress] == Status.Issued || whitelistUsers[userAddress] == Status.Pickup;
     }
 
     function topUp(address toUserAddress, address tokenAddress, uint256 amount) internal {
